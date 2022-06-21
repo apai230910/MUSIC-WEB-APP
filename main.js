b8c699c4fa08b16b7f251c0639f7153909ce7041
apai230910 <91840707+apai230910@users.noreply.github.com>
2022-06-21 17:34:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ song1 = "";
 
 song2 = "";
 
+leftWristX = "";
+
+leftWristY = "";
+
+rightWristY = "";
+
+rightWristX = "";
+
 function preload() {
 
  song1 = loadSound("music.mp3");
@@ -20,6 +28,34 @@ function setup() {
 
  video.hide();
  
+ poseNet = ml5.poseNet( video , modalLoaded);
+
+ poseNet.on( "pose", gotPoses );
+
+}
+
+function modalLoaded() {
+
+ console.log("PoseNet Intialized");
+
+}
+
+function gotPoses(results) {
+
+ if(results.length > 0) {
+
+ console.log(results);
+
+ leftWristX = results[0].pose.leftWrist.x;
+
+ leftWristY = results[0].pose.leftWrist.y;
+
+ rightWristY = results[0].pose.rightWrist.y;
+
+ rightWristX = results[0].pose.rightWrist.x;
+  
+}
+
 }
 
 function draw() {
